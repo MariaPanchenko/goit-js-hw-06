f08e9cd643c76a91079c4381f5982623e78ba24d
MariaPanchenko <marysa2502@gmail.com>
2023-06-03 17:15:20 +0300

    9 task

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const buttonColor = document.querySelector('button.change-color');
+const body = document.querySelector('body');
+const colorValue = document.querySelector('span.color');
+
+buttonColor.addEventListener('click', () => {
+  let color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  colorValue.textContent = color;
+});
