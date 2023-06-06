547e94e44d9e40577610742a0af0dc07a2ed1ff1
MariaPanchenko <marysa2502@gmail.com>
2023-06-06 14:21:43 +0300

    fix 5 and 7 tasks

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -4,7 +4,7 @@ const textOutput = document.querySelector('#name-output');
 textInput.addEventListener('input', (event) => {
   textOutput.textContent = event.target.value;
 
-  if (event.target.value === ' ') {
+  if (event.target.value === '') {
     textOutput.textContent = 'Anonymous';
   }
 });
--- src/js/task-07.js
+++ src/js/task-07.js
@@ -1,6 +1,6 @@
 const inputChange = document.querySelector('input#font-size-control');
 const mainText = document.querySelector('span#text');
 
-inputChange.addEventListener('change', (e) => {
+inputChange.addEventListener('input', (e) => {
   mainText.style.fontSize = `${e.target.value}px`;
 });
